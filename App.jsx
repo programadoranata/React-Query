2c3acffc0b6d83001febe368731658f1a4ca6991
programadoranata <giovannapgoncalves.04@gmail.com>
2026-01-29 11:07:20 -0300

    Initialize App component with TaskList

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import TaskList from './components/TaskList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Aqui é onde a mágica acontece. Remova o código do Vite e coloque isso: */}
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
