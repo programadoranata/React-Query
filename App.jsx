2b8021c1c0f94981bc45184a697498e5df2c44f2
programadoranata <giovannapgoncalves.04@gmail.com>
2026-01-29 11:31:50 -0300

    Clean up App component by removing comment
    Removed comment about Vite code and retained TaskList component.

--- App.jsx
+++ App.jsx
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* Aqui é onde a mágica acontece. Remova o código do Vite e coloque isso: */}
       <TaskList />
     </div>
   );
